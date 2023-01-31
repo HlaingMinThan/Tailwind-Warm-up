6e753e9987c230551162e149acf1054b9b411fad
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-01-31 14:19:06 +0630

    add tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 module.exports = {
   content: ['*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'sm': '480px',
+        // => @media (min-width: 480px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '976px',
+        // => @media (min-width: 976px) { ... }
+  
+        'xl': '1440',
+        // => @media (min-width: 1440) { ... }
+      },
+      colors : {
+        brightRed: 'hsl(12, 88%, 59%)',
+        brightRedLight: 'hsl(12, 88%, 69%)',
+        brightRedSupLight: 'hsl(12, 88%, 95%)',
+        darkBlue: 'hsl(228, 39%, 23%)',
+        darkGrayishBlue: 'hsl(227, 12%, 61%)',
+        veryDarkBlue: 'hsl(233, 12%, 13%)',
+        veryPaleRed: 'hsl(13, 100%, 96%)',
+        veryLightGray: 'hsl(0, 0%, 98%)',
+      }
+    },
   },
   plugins: [],
 }
